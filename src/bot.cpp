9a03739d1668ae96575ef5838e2cbac0cb4159e3
Cezary924 <cezary924@gmail.com>
2023-12-02 10:33:29 +0100

    HTTP session opening & closing

--- src/bot.cpp
+++ src/bot.cpp
@@ -1,8 +1,16 @@
-#include <iostream>
-using namespace std;
+#if defined(_WIN32)
+#include "http_client/http_windows.hpp"
+#else
+#include "http_client/http_unix.hpp"
+#endif
 
 int main()
 {
   cout << "Hello World!" << endl;
+
+  HINTERNET hSession = initialize_session();
+  cout << hSession << endl;
+  cout << close_session(&hSession) << endl;
+
   return 0;
 }
