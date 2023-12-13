ae1ac491f0b08689a7f8c46c14cc6a78b1ee367a
Cezary924 <cezary924@gmail.com>
2023-12-13 18:21:10 +0100

    Starting project over

--- src/http_client/http_unix.cpp
+++
@@ -1,1 +0,0 @@
-//TODO requires implementation
--- src/http_client/http_windows.hpp
+++
@@ -1,7 +0,0 @@
-#include <windows.h>
-#include <winhttp.h>
-#include <iostream>
-using namespace std;
-
-HINTERNET initialize_session();
-int close_session(HINTERNET *);
