74b3fb28e83cc1e7a35aa10a142fca9606f2bad5
Cezary924 <cezary924@gmail.com>
2024-01-02 14:35:29 +0100

    Add timer automation

--- src/automations.py
+++ src/automations.py
@@ -1,3 +1,11 @@
 from datetime import datetime, timedelta
 
 import database, log
+
+# check if device should be turned off
+def timer(automation: dict, accessories_database: database.Database) -> None:
+    if accessories_database.get_accessory_value(automation['uniqueId'], automation['characteristic']) == 1:
+        if accessories_database.get_accessory_date(automation['uniqueId']) + timedelta(seconds = int(automation['data']['period'])) < datetime.now():
+            accessories_database.set_accessory_value(automation['uniqueId'], automation['characteristic'], 0)
+            log.print_log('Timer', "'" + accessories_database.get_accessory_serviceName(automation['uniqueId']) + "' --- '" + automation['characteristic'] + "'")
+
